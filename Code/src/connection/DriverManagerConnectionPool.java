508c62d4291dc71c1646175f11f9621bf8961d24
00Joshua <luigicrisci1997@gmail.com>
2019-01-03 18:45:53 +0100

    Inserimento Progetto eclipse

---
+++ Code/src/connection/DriverManagerConnectionPool.java
@@ -0,0 +1,66 @@
+package connection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.LinkedList;
+import java.util.List;
+
+
+public class DriverManagerConnectionPool {
+
+	private static List<Connection> freeDbConnections;
+
+	static {
+		freeDbConnections = new LinkedList<Connection>();
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			System.out.println("DB driver not found:"+ e.getMessage());
+		} 
+	}
+	
+	private static synchronized Connection createDBConnection() throws SQLException {
+		Connection newConnection = null;
+		String ip = "localhost";
+		String port = "3306";
+		String db = "ottico";
+		String username = "root";
+		String password = "PentiumD";
+
+		newConnection = DriverManager.getConnection("jdbc:mysql://"+ ip+":"+ 
+				port+"/"+db+"?zeroDateTimeBehavior=convertToNull&useSSL=false", username, password);		
+
+		System.out.println("Create a new DB connection");
+		newConnection.setAutoCommit(false);
+		return newConnection;
+	}	
+	
+	public static synchronized Connection getConnection() throws SQLException {
+		Connection connection;
+
+		if (!freeDbConnections.isEmpty()) {
+			connection = (Connection) freeDbConnections.get(0);
+			freeDbConnections.remove(0);
+
+			try {
+				if (connection.isClosed())
+					connection = getConnection();
+			} catch (SQLException e) {
+				connection.close();
+				connection = getConnection();
+			}
+		} else {
+			connection = createDBConnection();		
+		}
+
+		return connection;
+	}
+	
+	public static synchronized void releaseConnection(Connection connection) 
+			throws SQLException {
+		if(connection != null) freeDbConnections.add(connection);
+	}	
+	
+	
+}
---
+++ Code/src/control/corso/ModCorsoServlet.java
@@ -0,0 +1,41 @@
+package control.corso;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ModCorsoServlet
+ */
+@WebServlet("/ModCorsoServlet")
+public class ModCorsoServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ModCorsoServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
